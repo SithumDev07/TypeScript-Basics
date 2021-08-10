3db20cdf909af5a7d6f8ae18bd421e6bfbf6c16b
SithumDev07 <sithum.dashantha@gmail.com>
2021-08-11 03:04:52 +0530

    Overview

---
+++ 01/index.js
@@ -0,0 +1,1 @@
+console.log('Hello World Without Any Type Errors');
---
+++ 01/index.ts
@@ -0,0 +1,8 @@
+console.log('Hello World Without Any Type Errors');
+
+let a = 5;
+let c = 87;
+
+if (a >= c) {
+    console.log('A is greater than c');
+}
